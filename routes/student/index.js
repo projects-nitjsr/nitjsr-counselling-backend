708838e17bb3a324437588056f878e129c0f98d0
Aman Jha <aman.jha21.aj@gmail.com>
2022-05-05 01:48:55 +0545

    routes changed for student

--- routes/student/index.js
+++ routes/student/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const controllers = require("../../controllers");
 
-router.get("/:id", controllers.user.getUserByRegNo);
+router.get("/:regNo", controllers.user.getStudentByRegNo);
 
 module.exports = router;
