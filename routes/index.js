df209b0bad6573aacc465f2ccc233d9bd9c6507c
Vinamra0 <singh001vinamra@gmail.com>
2022-05-16 14:03:04 +0545

    added admin routes

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,13 @@ const router = require("express").Router();
 const college = require("./college");
 const auth = require("./auth");
 const student = require("./student");
-<<<<<<< HEAD
 const admin = require("./admin");
-=======
 const result = require("./result");
->>>>>>> 0aeb86bca4d88fd1ddeb8eca3c70805762c30dcc
 
 router.use("/auth", auth);
 router.use("/student", student);
 router.use("/college", college);
-<<<<<<< HEAD
 router.use("/admin", admin);
-=======
 router.use("/result", result);
->>>>>>> 0aeb86bca4d88fd1ddeb8eca3c70805762c30dcc
 
 module.exports = router;
