05cdc9a170ee76f89668e746fd791d3aae92480c
ShruCmo <shruti.jha@caremonitor.com.au>
2022-05-16 20:37:30 +0530

    College Related APIs and seat matrix related documentation Added

--- controllers/seat_allocation.js/allocation_algo.js
+++ controllers/seat_allocation.js/allocation_algo.js
@@ -1,41 +1,11 @@
 'use strict';
-import Queue from './allocation_queue';
 
-var size = 505;
-var Ranking = Array(size).fill( Array(size).fill("0") );
-var InstitutePref = Array(size).fill( Array(size).fill("0") );
-var Next = Array(size).fill( Array(size).fill("0") );
-var Current = Array(size).fill( Array(size).fill("0") );
+const allocate = (seatMatrix,metricList, preferenceList) => {
+    modifiedMetricList = sortMeritListByRank(metricList); 
 
+    for(let studentIndex in metricList){
+        // To be Added Further
+     }
+ 
 
-const allocate = () => {
-    
-    var N, i, j, m, w;
-    N=505;
-    let q = new Queue();
-    for (i = 1; i <= N; i++)
-        for (j = 1; j <= N; j++)
-            Ranking[i][InstitutePref[i][j]] = j;
-    for (i = 1; i <= N; i++){
-        freeCandidate.enqueue(i);
-        Next[i] = 1;
-    }
-    while (!freeCandidate.isEmpty())    {
-        m = freeCandidate.peek();
-        w = ManPref[m][Next[m]++];
-        if (Current[w] == 0)   {
-            Current[w] = m;
-            freeCandidate.dequeue();
-        }
-        else if (Ranking[w][m] < Ranking[w][Current[w]])  {
-            freeCandidate.dequeue();
-            freeCandidate.enqueue(Current[w]);
-            Current[w] = m;
-        }
-    }
-    // (m, w) pairs in the stable matching
-    for (w = 1; w <= N; w++)
-        console.log(Current[w]+" "+w);
-
-    return 0;
 }
