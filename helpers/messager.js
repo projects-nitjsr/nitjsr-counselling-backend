0ac7567d32bafa454db67537b749f4137d4d11f9
sarthak2823 <sarthakgoel1234@gmail.com>
2022-05-16 15:19:46 +0530

    mailer and messager created

---
+++ helpers/messager.js
@@ -0,0 +1,36 @@
+
+const { CourierClient } =require("@trycourier/courier");
+const messager=async(phoneNumber,messageTitle,messageData)=>{
+
+ const courier = CourierClient(
+   { authorizationToken: process.env.CLOUDINARY_TOKEN});
+ 
+ const { requestId } = await courier.send({
+   message: {
+     to: {
+       phone_number: phoneNumber,
+     },
+     routing: {
+       method: "single",
+       channels: ["sms"]
+     },
+     channels : {
+       sms: {
+         providers: ["twilio"] 
+       }
+     },
+     content: {
+       title: messageTitle,
+       body: messageData,
+     }
+   }
+ });
+}
+module.exports=messager
+//driverCode
+// const messager=require ("../helpers/messager")
+// const test = async (req, res) => {
+//     await messager("+919636731474","hii","hello this is Sarthak Goel")
+//     res.send("message sent succesfully")
+// };
+// module.exports = test;
