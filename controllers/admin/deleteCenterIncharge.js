f78f116eb9cf9e7b9c0e1e6a6a4c48dcc654d9a2
Vinamra0 <singh001vinamra@gmail.com>
2022-05-16 14:45:39 +0545

    API for deleting center incharge created

--- controllers/admin/deleteCenterIncharge.js
+++ controllers/admin/deleteCenterIncharge.js
@@ -1,3 +1,14 @@
 const db = require("../../helpers/dbconnect");
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+  const centerInchargeEmail = req.body.email;
+
+  try {
+    await db.queryAsync("DELETE FROM admin WHERE email = ? ", [
+      centerInchargeEmail,
+    ]);
+    res.status(200).json({ success: true, message: "Center Incharge Deleted" });
+  } catch (err) {
+    res.status(404).json({ success: false, message: err.message });
+  }
+};
