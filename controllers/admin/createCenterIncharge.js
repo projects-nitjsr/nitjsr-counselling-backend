7df0cc9ac64298c0182e2a4f29a3596d4dbe3cc0
Vinamra0 <singh001vinamra@gmail.com>
2022-05-16 14:02:09 +0545

    logic for center Incharge creation route created

--- controllers/admin/createCenterIncharge.js
+++ controllers/admin/createCenterIncharge.js
@@ -1,3 +1,21 @@
 const db = require("../../helpers/dbconnect");
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+  const email = req.body.email;
+  const name = req.body.name;
+  const phone = req.body.phone;
+  const college = req.body.college;
+  const collegeEmail = req.body.collegeEmail;
+
+  try {
+    await db.queryAsync(
+      "INSERT INTO center_incharge (email , name , phone , college , college_email) values (? ,? ,?,?,?)",
+      [email, name, phone, college, collegeEmail]
+    );
+    res
+      .status(200)
+      .json({ success: true, message: "Center Incharge Created Successfully" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
