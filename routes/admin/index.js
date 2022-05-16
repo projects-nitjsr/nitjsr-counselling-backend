4f7f238a58eaee8a8eb048b75f0e294b0ac77550
Vinamra0 <singh001vinamra@gmail.com>
2022-05-16 15:30:44 +0545

    configured the routing to access the API on different routes

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const controllers = require("../../controllers");
 
-router.post("/", controllers.admin.createCenterIncharge);
+router.post("/createCenterIncharge", controllers.admin.createCenterIncharge);
+router.delete("/deleteCenterIncharge", controllers.admin.deleteCenterIncharge);
+router.get("/getCenterIncharge", controllers.admin.getCenterIncharge);
+router.post("/updateCenterIncharge", controllers.admin.updateCenterIncharge);
 
 module.exports = router;
