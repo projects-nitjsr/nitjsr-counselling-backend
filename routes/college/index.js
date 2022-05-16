22c82a5a2de05d07af5e2c99b7478f19f1c2e0fc
ShruCmo <shruti.jha@caremonitor.com.au>
2022-05-16 20:54:31 +0530

    Routes Added

--- routes/college/index.js
+++ routes/college/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const controllers = require("../../controllers");
 
-router.get("/:id", controllers.college.getCollegeById);
+router.get("/getcolleges", controllers.college.getCollegeList);
+router.get("/getcollege/:id", controllers.college.getCollegeById);
+router.get("/deletecollege/:id", controllers.college.deleteCollegeById);
+router.get("/updatecollege/:id", controllers.college.updateCollegeById);
 
 module.exports = router;
