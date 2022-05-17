4d415def471cd9094176100265cd6e1e296ddb8b
vaibhav kedia <vaibhavkedia24@gmail.com>
2022-05-17 22:51:01 +0530

    added pagination

--- controllers/result/getResult.js
+++ controllers/result/getResult.js
@@ -1,8 +1,15 @@
 const db = require("../../helpers/dbconnect");
 
 const getResult = async (req, res) => {
+  let page = parseInt(req.query.page) || 1;
+  page--;
+  const limit = parseInt(req.query.limit) || 20;
+  const offset = page * limit;
   try {
-    const result = await db.queryAsync("SELECT * FROM result", []);
+    const result = await db.queryAsync(
+      "SELECT * FROM result ORDER BY regno LIMIT = ? OFFSET = ?",
+      [limit, offset]
+    );
 
     if (result.length == 0) {
       throw new Error("No results found!");
