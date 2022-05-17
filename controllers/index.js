11b94b1d70b8fa2a45e41319ef48898d5cf70021
Ankit Raj <2019ugcs001@nitjsr.ac.in>
2022-05-17 08:57:40 +0530

    controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,8 @@ module.exports = {
   auth: {
     adminLogin: require("./auth/admin/login"),
     studentLogin: require("./auth/login"),
+    adminForgotPassword: require("./auth/admin/forgotPassword"),
+    adminResetPassword: require("./auth/admin/resetPassword"),
   },
   student: {
     getStudentByRegNo: require("./student/getStudentByRegNo"),
