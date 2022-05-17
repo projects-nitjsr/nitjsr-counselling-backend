e2c87a2f2fe3396172312c0c9181ed983f324a00
Vinamra0 <singh001vinamra@gmail.com>
2022-05-17 17:17:05 +0545

    added check for centerIncharge existence on the update route

--- controllers/admin/updateCenterIncharge.js
+++ controllers/admin/updateCenterIncharge.js
@@ -11,34 +11,45 @@ module.exports = async (req, res) => {
   const profile_image_url = req.body.profile_image_url;
 
   try {
-    if (name) {
-      await db.queryAsync(
-        "UPDATE center_incharge SET name = ? WHERE email = ?",
-        [name, email]
-      );
+    const centerInchargeExist = await db.queryAsync(
+      "SELECT EXISTS(SELECT 1 FROM admin WHERE email = ? )",
+      [email]
+    );
+    if (centerInchargeExist > 0) {
+      if (name) {
+        await db.queryAsync(
+          "UPDATE center_incharge SET name = ? WHERE email = ?",
+          [name, email]
+        );
+      }
+      if (phone) {
+        await db.queryAsync(
+          "UPDATE center_incharge SET phone = ? WHERE email = ?",
+          [phone, email]
+        );
+      }
+      if (collegeEmail) {
+        await db.queryAsync(
+          "UPDATE center_incharge SET college_email = ? WHERE email = ?",
+          [collegeEmail, email]
+        );
+      }
+      if (profile_image_url) {
+        await db.queryAsync(
+          "UPDATE admin SET profile_image_url = ? WHERE email = ?",
+          [profile_image_url, email]
+        );
+      }
+      res.status(200).json({
+        success: true,
+        message: "Details for Center Incharge Updated!! ",
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Center Incharge doesnot exist! ",
+      });
     }
-    if (phone) {
-      await db.queryAsync(
-        "UPDATE center_incharge SET phone = ? WHERE email = ?",
-        [phone, email]
-      );
-    }
-    if (collegeEmail) {
-      await db.queryAsync(
-        "UPDATE center_incharge SET college_email = ? WHERE email = ?",
-        [collegeEmail, email]
-      );
-    }
-    if (profile_image_url) {
-      await db.queryAsync(
-        "UPDATE admin SET profile_image_url = ? WHERE email = ?",
-        [profile_image_url, email]
-      );
-    }
-    res.status(200).json({
-      success: true,
-      message: "Details for Center Incharge Updated!! ",
-    });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
