1d6265a9f5069713f2da88244edf4974e9913252
alok-dotcom <papamummyalok@gmail.com>
2022-05-18 11:43:50 +0530

    getstudentbyveryfyingcollege and getstudentsbyveryfyingofficer routes added

--- routes/student/index.js
+++ routes/student/index.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const controllers = require("../../controllers");
 
+router.get(
+  "/verifyingcollege",
+  controllers.student.getStudentsByVerifingCollege
+);
+router.get(
+  "/verifyingofficer",
+  controllers.student.getStudentsByVeryfingOfficer
+);
 router.get("/:regNo", controllers.student.getStudentByRegNo);
 router.get("/", controllers.student.getStudents);
 router.get("/getstudentstatus/:regNo", controllers.student.getStudentStatus);
