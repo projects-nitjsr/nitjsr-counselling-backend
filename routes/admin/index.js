7225c08ba87c85ff336ce3044dc79c8805e8778e
Aman Jha <aman.jha21.aj@gmail.com>
2022-05-18 15:19:59 +0545

    verifying officer route added

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const centerIncharge = require("./centerIncharge");
+const verifyingOfficer = require("./verifyingOfficer");
+
 router.use("/centerIncharge", centerIncharge);
+router.use("/verifyingOfficer", verifyingOfficer);
 
 module.exports = router;
