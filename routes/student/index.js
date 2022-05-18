0149feae395ec6ee4650ca332d7c24af833a1789
tarique4a <tarique.nitjsr77@gmail.com>
2022-05-18 12:20:20 +0530

    verify,reject,confirm,deny

--- routes/student/index.js
+++ routes/student/index.js
@@ -1,18 +1,18 @@
 const router = require("express").Router();
 const controllers = require("../../controllers");
 
-router.get("/:regNo", controllers.student.getStudentByRegNo);
 router.get("/", controllers.student.getStudents);
-router.get("/getstudentstatus/:regNo", controllers.student.getStudentStatus);
-router.put(
-  "/updatestudentstatus/:regNo",
-  controllers.student.updateStudentStatus
-);
-router.delete("/deletestudent/:regNo", controllers.student.deleteStudent);
 
 router.put("/verify", controllers.verify.verifyStudent);
 router.put("/reject", controllers.verify.rejectStudent);
 router.put("/confirm", controllers.verify.confirmDecision);
 router.put("/deny", controllers.verify.denyDecision);
+router.get("/:regNo", controllers.student.getStudentByRegNo);
+router.get("/getstudentstatus/:regNo", controllers.student.getStudentStatus);
+router.put(
+  "/updatestudentstatus/:regNo",
+  controllers.student.updateStudentStatus
+  );
+  router.delete("/deletestudent/:regNo", controllers.student.deleteStudent);
 
 module.exports = router;
