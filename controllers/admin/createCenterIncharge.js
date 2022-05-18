8e637a01d4ea8573fc3c0056368a1ee3711dba50
Vinamra0 <singh001vinamra@gmail.com>
2022-05-18 23:30:05 +0545

    optimized the createCenterIncharge route

--- controllers/admin/createCenterIncharge.js
+++ controllers/admin/createCenterIncharge.js
@@ -1,36 +1,41 @@
 const db = require("../../helpers/dbconnect");
 const generator = require("generate-password");
+const imageUploader = require("../../helpers/imageHandler");
 module.exports = async (req, res) => {
   const email = req.body.email;
   const name = req.body.name;
   const phone = req.body.phone;
   const college = req.body.college;
+  const image = req.file;
+  const password = req.body.password;
   const collegeEmail = req.body.collegeEmail;
 
   try {
-    await db.queryAsync(
-      "INSERT INTO admin ( email , designation , profile_image_url) values (? ,? ,?)",
-      [email, "Center Incharge", ""]
-    );
-    await db.queryAsync(
-      "INSERT INTO center_incharge (email , name , phone , college , college_email) values (? ,? ,?,?,?)",
-      [email, name, phone, college, collegeEmail]
-    );
-    // create a random password
+    const imageUrl = await imageUploader.uploadImage(image);
 
-    const password = generator.generate({
-      length: 10,
-      numbers: true,
-      symbols: true,
-      lowercase: true,
-      uppercase: true,
-      strict: true,
-    });
-    // assign the password to the center incharge
     await db.queryAsync(
-      "INSERT INTO admin_credentials (password) VALUES (?) WHERE email = ? ",
-      [password, email]
+      ` SET autocommit =0 ;
+START TRANSACTION;
+INSERT INTO admin ( email , designation , profile_image_url) values (? ,? ,?);
+INSERT INTO center_incharge (email , name , phone , college , college_email) values (? ,? ,?,?,?);
+INSERT INTO admin_credentials (email ,password, token) VALUES (?,?,NULL);
+COMMIT;
+SET autocommit =1
+`,
+      [
+        email,
+        "ci",
+        imageUrl,
+        email,
+        name,
+        phone,
+        college,
+        collegeEmail,
+        email,
+        password,
+      ]
     );
+
     // send the password to the center incharge email
 
     res
