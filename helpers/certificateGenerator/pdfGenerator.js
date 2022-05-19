a4cf437666385ac159e31269483d87cb166b7340
Aman Jha <aman.jha21.aj@gmail.com>
2022-05-19 21:03:06 +0545

    minor changes to pdf handler

--- helpers/certificateGenerator/pdfGenerator.js
+++ helpers/certificateGenerator/pdfGenerator.js
@@ -7,17 +7,20 @@ const { uploadPdf } = require("../pdfHandler");
 module.exports = async () => {
   let data = {};
   const pdf = {};
+  try {
+    const template = hb.compile(certificateTemplete, { strict: true });
+    const html = template(data);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
 
-  const template = hb.compile(certificateTemplete, { strict: true });
-  const html = template(data);
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setContent(html);
+    pdf.buffer = await page.pdf({ format: "A4" });
+    await browser.close();
+    pdf.mimetype = "application/pdf";
 
-  pdf.buffer = await page.pdf({ format: "A4" });
-  await browser.close();
-  pdf.mimetype = "application/pdf";
-
-  const pdfUrl = await uploadPdf(pdf);
-  return pdfUrl;
+    const pdfUrl = await uploadPdf(pdf);
+    return pdfUrl;
+  } catch (err) {
+    console.error(err);
+  }
 };
