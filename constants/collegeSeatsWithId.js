8240a40347e82392350f3facb936e87a0023ad07
BANGBANGROX <lakshya0809bang@gmail.com>
2022-05-19 23:40:37 +0530

    Added constants

---
+++ constants/collegeSeatsWithId.js
@@ -0,0 +1,36 @@
+const collegeSeatsWithId = {
+  1: {
+    general_seats: 8,
+    obc_seats: 5,
+    sc_seats: 3,
+    st_seats: 2,
+    ews_seats: 1,
+    pwd_general_seats: 1,
+  },
+  2: {
+    general_seats: 10,
+    obc_seats: 5,
+    sc_seats: 3,
+    st_seats: 2,
+    ews_seats: 1,
+    pwd_general_seats: 0,
+  },
+  3: {
+    general_seats: 10,
+    obc_seats: 5,
+    sc_seats: 3,
+    st_seats: 2,
+    ews_seats: 1,
+    pwd_general_seats: 0,
+  },
+  4: {
+    general_seats: 10,
+    obc_seats: 5,
+    sc_seats: 3,
+    st_seats: 2,
+    ews_seats: 1,
+    pwd_general_seats: 0,
+  },
+};
+
+module.exports = collegeSeatsWithId;
