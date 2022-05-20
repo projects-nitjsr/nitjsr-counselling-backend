f628afc98d47c2c5a1552d95944a43aacadbdc7d
BANGBANGROX <lakshya0809bang@gmail.com>
2022-05-20 16:09:46 +0530

    Added validation to student routes

--- routes/student/index.js
+++ routes/student/index.js
@@ -1,15 +1,18 @@
 const router = require("express").Router();
 const controllers = require("../../controllers");
+const validationSchema = require("./validationSchema");
+const validation = require("../../middlewares/validation");
 
 router.get(
   "/verifyingcollege",
+  validation(validationSchema.getStudentsByVerifyingCollegeValidation),
   controllers.student.getStudentsByVerifingCollege
 );
 router.get(
   "/verifyingofficer",
+  validation(validationSchema.getStudentsByVerifingOfficerValidation),
   controllers.student.getStudentsByVeryfingOfficer
 );
-
 router.get("/", controllers.student.getStudents);
 router.put("/verify", controllers.verify.verifyStudent);
 router.put("/reject", controllers.verify.rejectStudent);
@@ -19,8 +22,9 @@ router.get("/:regNo", controllers.student.getStudentByRegNo);
 router.get("/getstudentstatus/:regNo", controllers.student.getStudentStatus);
 router.put(
   "/updatestudentstatus/:regNo",
+  validation(validationSchema.updateStudentStatusValidation),
   controllers.student.updateStudentStatus
-  );
+);
 router.delete("/deletestudent/:regNo", controllers.student.deleteStudent);
 
 module.exports = router;
