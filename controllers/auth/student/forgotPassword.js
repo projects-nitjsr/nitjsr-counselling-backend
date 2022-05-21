35ccfe6e3f324c6abc94a51494e5dda3a2c451c5
vaibhav kedia <vaibhavkedia24@gmail.com>
2022-05-21 23:29:46 +0530

    changed input req to regno

--- controllers/auth/student/forgotPassword.js
+++ controllers/auth/student/forgotPassword.js
@@ -2,33 +2,33 @@ const db = require("../../../helpers/dbconnect");
 const jwt = require("jsonwebtoken");
 const forgotPassword = async (req, res) => {
   try {
-    const { email } = req.body;
-    if (!email) {
+    const { regno } = req.body;
+    if (!regno) {
       res.status(400).json({
         message: "Missing parameters",
         status: 0,
       });
     } else {
-      const searchStudent = `SELECT * FROM student_credentials WHERE email = ?`;
-      const student = db.queryAsync(searchStudent, [email]);
-      if (!student) {
+      const searchStudent = `SELECT * FROM student WHERE regno = ?`;
+      const student = await db.queryAsync(searchStudent, [regno]);
+      if (student.length == 0) {
         res.status(200).json({
-          message: "Not found",
+          message: "Student not found",
           status: 0,
         });
       } else {
         const token = jwt.sign(
           {
-            email: student.email,
+            regno: student[0].regno,
           },
           process.env.STUDENT_FORGOT_PASSWORD_SECRET,
-          { expiresIn: "1" }
+          { expiresIn: "1h" }
         );
 
         //send email
         //url = process.env.FRONTEND_URL/reset?t=token
         res.status(200).json({
-          message: `Email sent to ${email} with an url to reset password`,
+          message: `Email sent to ${student[0].email} with an url to reset password`,
           status: 1,
           token: token,
         });
@@ -36,7 +36,7 @@ const forgotPassword = async (req, res) => {
     }
   } catch (err) {
     res.status(500).json({
-      message: "Internal Server Error" + err.message,
+      message: "Internal Server Error " + err.message,
       status: 0,
     });
   }
