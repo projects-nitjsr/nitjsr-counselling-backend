5033b45646ef4f24a9409b407a6d758396576cb4
Vinamra0 <singh001vinamra@gmail.com>
2022-05-28 12:02:05 +0545

    removed user from validation as it is to be obtained from auth

--- routes/notice/validationSchema.js
+++ routes/notice/validationSchema.js
@@ -2,7 +2,6 @@ const Joi = require("@hapi/joi");
 
 const createNoticeValidation = Joi.object({
   notice: Joi.string().min(50).required(),
-  user: Joi.string().required(),
 });
 
 module.exports = {
