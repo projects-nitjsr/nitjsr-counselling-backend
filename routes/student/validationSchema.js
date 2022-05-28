c65ca38872d6c03282e8ef3f0d64b6edc6f5e333
Vinamra0 <singh001vinamra@gmail.com>
2022-05-28 12:02:33 +0545

    created validation

--- routes/student/validationSchema.js
+++ routes/student/validationSchema.js
@@ -1,25 +1,61 @@
 const Joi = require("@hapi/joi");
 
-const getStudentsByVerifyingCollegeValidation = Joi.object({
+const getstudentsbyverifingcollegeValidation = Joi.object({
   email: Joi.string().email().required(),
 });
 
-const getStudentsByVerifingOfficerValidation = Joi.object({
+const getstudentsbyverifingofficerValidation = Joi.object({
   email: Joi.string().email().required(),
+});
+
+const studentActionValidation = Joi.object({
+  regno: Joi.string().required(),
+  studentAction: Joi.string().required(),
+});
+
+const updateStudentDataValidation = Joi.object({
+  regno: Joi.string().required(),
+  name: Joi.string(),
+  email: Joi.string().email(),
+  category: Joi.string(),
 });
 
 const updateStudentVerifyingCollegeValidation = Joi.object({
-  verifyingCollegeWithRegNo: Joi.array().required(),
+  verifyingCollegeWithRegNo: Joi.array().items(Joi.string()),
 });
 
 const updateStudentVerifyingOfficerValidation = Joi.object({
-  regNo: Joi.string().required(),
-  verifyingOfficer: Joi.string().email().required(),
+  regno: Joi.string().required(),
+  verifyingOfficerEmail: Joi.string().email().required(),
+});
+
+const confirmDecisionValidation = Joi.object({
+  regno: Joi.string().required(),
+});
+
+const denyDecisionValidation = Joi.object({
+  regno: Joi.string().required(),
+});
+
+const rejectStudentValidation = Joi.object({
+  regno: Joi.string().required(),
+  email: Joi.string().email().required(),
+  message: Joi.string().min(30).required(),
+  categoryRejection: Joi.string().required(),
+});
+const verifyStudentValidation = Joi.object({
+  regno: Joi.string().required(),
 });
 
 module.exports = {
-  getStudentsByVerifyingCollegeValidation,
-  getStudentsByVerifingOfficerValidation,
+  getstudentsbyverifingcollegeValidation,
+  getstudentsbyverifingofficerValidation,
+  studentActionValidation,
+  updateStudentDataValidation,
   updateStudentVerifyingCollegeValidation,
   updateStudentVerifyingOfficerValidation,
+  confirmDecisionValidation,
+  denyDecisionValidation,
+  rejectStudentValidation,
+  verifyStudentValidation,
 };
